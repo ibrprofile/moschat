13fc80f0e25b04169779ac19cc854298b9aff05d
Azamat Ibragimov <ibr.azamat.0@gmail.com>
2026-09-16 21:46:55 +0300

    Add files via upload
    Первая версия

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => null,
+        'name' => 'moschat/platform',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'moschat/platform' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => null,
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'dev_requirement' => false,
+        ),
+    ),
+);
